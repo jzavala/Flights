5b79f894c4c650e6a18eafe70411313f19b3c127
jzavala <josh.zavala@gmail.com>
2014-12-02 10:32:41 -0600

    Re-sync the application
    Git folder did not sync properly. Re-syncing the program to the master

---
+++ FlightApplication/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace FlightApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            SqlConnection sqlConnection1 = new
+            SqlConnection(ConfigurationManager.ConnectionStrings["Flights"].ToString());
+
+            //SqlConnection sqlConnection1 = new SqlConnection("Flights");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT * FROM dbo.flights";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+            // Data is accessible through the DataReader object here.
+
+            sqlConnection1.Close();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FlightApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlightApplication.MvcApplication" Language="C#" %>
